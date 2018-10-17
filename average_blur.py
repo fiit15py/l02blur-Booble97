e3d98de895ffead35c280362150f09f2bef37959
Booble97 <36526376+Booble97@users.noreply.github.com>
2018-10-17 17:21:20 +0900

    Add files via upload
    add average v 2.0

--- average_blur.py
+++ average_blur.py
@@ -2,6 +2,19 @@ from PIL import Image
 from math import pi, log, exp
 import numpy as np
 
+img = Image.open('darwin.png')
+img.load()
+
+print("Image size is", img.size)
+print("Image mode is", img.mode)
+
+print("getdata[1]=", img.getdata()[1])
+a = np.array(img, dtype=np.uint8).reshape(img.size[::-1])
+print("a[0,1]=", a[0,1])
+
+b = a[900:1150, 900:1100]
+pic = Image.fromarray(b)
+
 def average_blur(img, r):
     w, h = img.size
     a = np.array(img.getdata(), dtype=np.uint8).reshape(h, w)
@@ -20,20 +33,4 @@ def average_blur(img, r):
                     
             b[i,j] = s / n#(2*r+1)**2
     return Image.fromarray(b)
-    average_blur(Image.fromarray(b),3)
-
-
-img = Image.open('darwin.png')
-img.load()
-
-print("Image size is", img.size)
-print("Image mode is", img.mode)
-
-print("getdata[1]=", img.getdata()[1])
-a = np.array(img, dtype=np.uint8).reshape(img.size[::-1])
-print("a[0,1]=", a[0,1])
-
-b = a[900:1150, 900:1100]
-pic = Image.fromarray(b)
-
-blur_np(pic,16).show()
+#blur_np(pic,16).show()
